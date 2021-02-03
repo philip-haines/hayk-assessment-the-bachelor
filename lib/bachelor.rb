c5238c30b8991c65b98074bb2db04600e2d9497e
Phil Haines <phil.haines92@gmail.com>
2021-02-02 21:09:39 -0700

    commit tree 4594bf344f5b35b28c5af06d00fc40c3c1b4b9c7

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,40 @@
+require 'pry'
+
+require 'json'
+file = File.read('./spec/fixtures/contestants.json')
+data = JSON.parse(file)
+
+def get_all_contenstants(data)
+  data.values.flatten
+end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].filter do |person|
+    person["status"] == "Winner"
+  end
+  winner[0]["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  lexicon = get_all_contenstants(data)
+  contestant = lexicon.find do |person|
+    person["occupation"] == occupation
+  end
+  contestant["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  lexicon = get_all_contenstants(data)
+  lexicon.filter { |person| person["hometown"] == hometown}.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant = get_all_contenstants(data).find do |person|
+    person["hometown"] == hometown
+  end
+  contestant["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  (data[season].map { |person| person["age"].to_f}.sum/ data[season].length).round
 end
